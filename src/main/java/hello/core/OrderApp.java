b129bb79efbce47913b4dee8de665118b9ea6fe1
junaem <junh9547@gmail.com>
2022-01-27 02:26:01 +0900

    220127 ljh

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
@@ -10,7 +11,7 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberServiceImpl memberService = new MemberServiceImpl();
+        MemberService memberService = new MemberServiceImpl();
         OrderService orderService = new OrderServiceImpl();
 
         Long memberId = 1L;
@@ -19,5 +20,6 @@ public class OrderApp {
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
         System.out.println("order = " + order);
+        System.out.println("order = " + order.calculatePrice());
     }
 }
